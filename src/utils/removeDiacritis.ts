5216386e89c0c725014283c9b6ace7696e2168df
marc <xbrcvar@gmail.com>
2022-01-18 15:52:49 -0300

    fix: verificar se o valor é uma string

--- src/utils/removeDiacritis.ts
+++ src/utils/removeDiacritis.ts
@@ -1,3 +1,3 @@
 export default function removeDiacritics (value: string) {
-  return value.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase()
+  return typeof value === 'string' ? value.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase() : ''
 }
