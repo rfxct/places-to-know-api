7752e3e477cf986c65eed1dbe8a11f34255fb073
marc <xbrcvar@gmail.com>
2022-01-18 18:39:22 -0300

    docs: dist folder

--- dev-tools.js
+++ dev-tools.js
@@ -2,13 +2,13 @@ const fs = require('fs')
 const path = require('path')
 
 const [, , command, ...args] = process.argv
-const models = fs.readdirSync('./templates')
+const models = fs.readdirSync('./dist/templates')
 
 if (command.toLowerCase().startsWith('create')) {
   const structure = capitalize(command.split(':').pop())
   if (!models.includes(structure)) die('Estrutura inválida')
 
-  const model = fs.readFileSync(`./templates/${structure}`, { encoding: 'utf-8' })
+  const model = fs.readFileSync(`./dist/templates/${structure}`, { encoding: 'utf-8' })
   return createStructure(structure, model.toString())
 } else {
   die('Nenhum comando especificado')
