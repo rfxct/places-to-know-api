fb6d2702d31216525690665dd8470cf70d667488
marc <xbrcvar@gmail.com>
2022-01-18 19:25:51 -0300

    feat: buscar por uma foto diferente da atual na API do Unsplash

--- src/apis/UnsplashAPI.ts
+++ src/apis/UnsplashAPI.ts
@@ -1,3 +1,4 @@
+import IUnsplashPhoto from '@interfaces/IUnsplashPhoto'
 import APIWrapper from '@structures/APIWrapper'
 
 const UnsplashAPI = class extends APIWrapper {
@@ -10,6 +11,12 @@ const UnsplashAPI = class extends APIWrapper {
 
   public async getPhoto (query: string) {
     return this.request('GET', '/search/photos', { params: { query } })
+  }
+
+  public async getUniquePhoto (query: string, fullUrl: string) {
+    const { data } = await this.getPhoto(query)
+
+    return data.results.find((d: IUnsplashPhoto) => d.urls.full !== fullUrl) || data.results.photos[0]
   }
 
   public async getRandomPhoto (query: string) {
